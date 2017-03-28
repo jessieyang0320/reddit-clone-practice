763e351159482c3bad4a69f2294aef7875516200
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-03-28 19:23:15 -0400

    association between link and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328223558) do
+ActiveRecord::Schema.define(version: 20170328231958) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title",      null: false
     t.string   "url",        null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
